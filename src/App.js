6cabe3a525d6e3d0c1e0dd583bc06d30d00cb5c3
Dory <enfzls1212@gmail.com>
2020-07-29 15:38:12 +0900

    App.js 두번째 커밋

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          리액트를 배워보쟈~~~
+          리액트를 또다시 배워보쟈~~
         </a>
       </header>
     </div>
